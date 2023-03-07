2b51af152509a947d2c64d953daedab827539dc1
AmeerSafdar <ameeersafdar786@gmail.com>
2023-03-07 22:10:16 +0500

    first commit

--- lib/repository/api_data_repo/api_data_implements.dart
+++ lib/repository/api_data_repo/api_data_implements.dart
@@ -11,44 +11,40 @@ class PostRepository extends APIData {
   API _api = API();
   Uint8List? imgs;
 
-@override
-Future<List<UserData>> fetchPosts() async {
+  @override
+  Future<List<UserData>> fetchPosts() async {
     try {
+      List<Map<String, dynamic>> usersList = await dbHelper.query();
 
-      List<Map<String, dynamic>> usersList =await dbHelper.query();
-
-      if ( usersList.isEmpty || usersList==null || usersList==[] ) {
-
-           Response response = await _api.sendRequest.get(CommonKey.user_key);
-           List<dynamic> _users = response.data[CommonKey.data_key];
-           List<UserData> _usersApi= _users.map((_users) => UserData.fromJson(_users)).toList();
-           for (var element in _usersApi) {
-              imgs = await getNetworkImage(element.avatar.toString());
-              dbHelper.insert(
-                UserData(
-                  first_name: element.first_name,
-                  last_name: element.last_name,
-                  img: imgs
-                )
-                );
-            }
-           usersList =await dbHelper.query();
+      if (usersList.isEmpty || usersList == null || usersList == []) {
+        Response response = await _api.sendRequest.get(CommonKey.user_key);
+        List<dynamic> _users = response.data[CommonKey.data_key];
+        List<UserData> _usersApi =
+            _users.map((_users) => UserData.fromJson(_users)).toList();
+        for (var element in _usersApi) {
+          imgs = await getNetworkImage(element.avatar.toString());
+          dbHelper.insert(UserData(
+              first_name: element.first_name,
+              last_name: element.last_name,
+              img: imgs));
+        }
+        usersList = await dbHelper.query();
       }
 
-    final userData=usersList.map((usersList) => UserData.fromJson(usersList)).toList();
-    return userData;
-    }
-
-    catch(ex) {
+      final userData =
+          usersList.map((usersList) => UserData.fromJson(usersList)).toList();
+      return userData;
+    } catch (ex) {
       throw ex;
     }
   }
 
-@override
- Future<Uint8List> getNetworkImage(String imgPath) async {
-      Uint8List _base64 = (await NetworkAssetBundle(Uri.parse(imgPath)).load(imgPath))
-                          .buffer.asUint8List();
-      return _base64;
-}
-
+  @override
+  Future<Uint8List> getNetworkImage(String imgPath) async {
+    Uint8List _base64 =
+        (await NetworkAssetBundle(Uri.parse(imgPath)).load(imgPath))
+            .buffer
+            .asUint8List();
+    return _base64;
+  }
 }
